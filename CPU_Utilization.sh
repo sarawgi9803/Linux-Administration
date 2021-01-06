c4327e9f632a019418704c151e01ec4463960ad3
Kushal Sarawgi <58667006+sarawgi9803@users.noreply.github.com>
2021-01-06 18:35:52 -0500

    Create CPU_Utilization.sh

---
+++ CPU_Utilization.sh
@@ -0,0 +1,18 @@
+CPU_USAGE=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk '{ print $8 }' | cut -c1-2)
+if [ $CPU_USAGE -le 10 ]; then
+DATE=$(date "+%F %H:%M:%S")
+CPU_USAGE1="$DATE CPU: $CPU_USAGE"
+echo "CRITICAL $CPU_USAGE1% Idle" >> /opt/cpusage.out
+cat /opt/cpusage.out |tail -5 > /tmp/cpusage.tmp
+mail -s "CPU Utilization of `hostname`" EMAILADDRESS < /tmp/cpusage.tmp
+elif [ $CPU_USAGE -ge 10 ] && [ $CPU_USAGE -le 20 ]; then
+DATE=$(date "+%F %H:%M:%S")
+CPU_USAGE1="$DATE CPU: $CPU_USAGE"
+echo "WARNING $CPU_USAGE1% Idle" >> /opt/cpusage.out
+cat /opt/cpusage.out |tail -5 > /tmp/cpusage.tmp
+mail -s "CPU Utilization of `hostname`" EMAILADDRESS < /tmp/cpusage.tmp
+else
+DATE=$(date "+%F %H:%M:%S")
+CPU_USAGE1="$DATE CPU: $CPU_USAGE"
+echo "OK $CPU_USAGE1% Idle" >> /opt/cpusage.out
+fi
